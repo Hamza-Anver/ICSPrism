3d978731ed9823811a4052fe45187416ab442ec6
Hamza Anver <hamzaanver1234@gmail.com>
2026-05-09 23:04:23 +0400

    FIX added compile if .so changed

--- icsprism/prism-cov/build.rs
+++ icsprism/prism-cov/build.rs
@@ -4,6 +4,12 @@ fn main() {
     let lib_name = std::env::var("PRISM_LIB_NAME")
         .expect("PRISM_LIB_NAME must be set to the library name without lib prefix or .so suffix");
 
+    if let Ok(dir) = std::env::var("PRISM_LIB_DIR") {
+        if let Ok(name) = std::env::var("PRISM_LIB_NAME") {
+            println!("cargo:rerun-if-changed={}/lib{}.so", dir, name);
+        }
+    }
+
     println!("cargo:rustc-link-search=native={}", lib_dir);
     println!("cargo:rustc-link-lib=dylib={}", lib_name);
     println!("cargo:rustc-link-arg=-Wl,-rpath,{}", lib_dir);
