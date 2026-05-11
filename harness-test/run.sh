c1240a4829b53c15157458edd6bf2673dfa0d240
Hamza Anver <hamzaanver1234@gmail.com>
2026-05-11 11:13:51 +0000

    FEAT added sanity test and removed unneeded code

--- harness-test/run.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# harness-test/run.sh
-WORKSPACE=$(cd "$(dirname "$0")/.." && pwd)
-LIB_DIR="$WORKSPACE/benchmarks/out"
-
-RUSTFLAGS="-L $LIB_DIR -l dylib=harness_test -C link-arg=-Wl,-rpath,$LIB_DIR" \
-    cargo run --bin harness-test
